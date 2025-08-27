95a5859b6f46b578986736bdc5acb62be6f0ee63
RicardoJTrulloG <ricardotg66@gmail.com>
2025-08-26 20:29:32 -0500

    add modal form

--- reflexapp/reflexapp.py
+++ reflexapp/reflexapp.py
@@ -24,6 +24,9 @@ class State(rx.State):
         ),
     ]
 
+    def add_user(self, form_data: dict):
+        self.user.append(User(**form_data))
+
 def showUser(user: User):
     return rx.table.row(
         rx.table.cell(user.name),
@@ -31,22 +34,84 @@ def showUser(user: User):
         rx.table.cell(user.gender),
     )
 
+def form():
+    return rx.dialog.root(
+            rx.dialog.trigger(
+                rx.button(
+                    rx.icon("plus", size=26),
+                    rx.text("add User", size="4"),
+            ),
+        ),
+        rx.dialog.content(
+            rx.dialog.title(
+                "Añadir un nuevo usuario",
+            ),
+            rx.dialog.description(
+                "Llenar la información del usuario",
+            ),
+        
+            rx.form(
+                rx.flex(
+                    rx.input(
+                        placeholder="Nombre", 
+                        name="name", 
+                        required=True
+                    ),
+                    rx.input(
+                        placeholder="Email", 
+                        name="email",
+                    ),
+                    rx.select(
+                        ['male','female'],
+                        placeholder="gender",
+                        name="gender"
+                    ),
+                    rx.flex(
+                        rx.dialog.close(
+                            rx.button(
+                                "Cancel",
+                                variant="soft",
+                                color_scheme="gray",
+                            ),
+                        ),
+                        rx.dialog.close(
+                            rx.button("Submit", type="submit"),
+                        ),
+                        spacing="3",
+                        justify="end",
+                    ),
+                    direction="column",
+                    spacing="4", 
+                ),
+                on_submit=State.add_user,
+                reset_on_submit=False,       
+            ),
+            max_width="450px",
+        ),
+    )
+    
+
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.table.root(
-        rx.table.header(
-            rx.table.row(
-                rx.table.column_header_cell("Nombre"),
-                rx.table.column_header_cell("Email"),
-                rx.table.column_header_cell("Genero"),
+    return rx.vstack(
+        form(),
+        rx.table.root(
+            rx.table.header(
+                rx.table.row(
+                    rx.table.column_header_cell("Nombre"),
+                    rx.table.column_header_cell("Email"),
+                    rx.table.column_header_cell("Genero"),
+                ),
             ),
+            rx.table.body(
+                rx.foreach(State.user, showUser),
+            ),
+            variant = "surface",
+            size = "3",
         ),
-        rx.table.body(
-            rx.foreach(State.user, showUser),
-        ),
-        variant = "surface",
-        size = "3",
+        justify="center",
+        align="center",
     )
 
 app = rx.App()
